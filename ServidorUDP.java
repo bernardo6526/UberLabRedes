1c5f655a0c4df1894fc3ee9561cc2656a8b79137
Bernardo6526 <bernardo6526@gmail.com>
2021-03-30 21:12:46 -0300

    Thread sync working

--- ServidorUDP.java
+++ ServidorUDP.java
@@ -6,22 +6,19 @@ public class ServidorUDP {
 	// METODO PRINCIPAL DA CLASSE
 	public static void main(String args[]) {
 		try {
-			// Porta na qual o servidor ira "escutar"
-			int ServerPort = 6000;
-
-			// ESTABELECE UM SERVIÇO UDP NA PORTA ESPECIFICADA
-			DatagramSocket serverds = new DatagramSocket(ServerPort);
-			System.out.println("-S- Servidor estabelecendo servico UDP (P:" + ServerPort + ")...");
-
-			// Abre a conexao no clienteHandler
-			Thread t = new ClienteHandler(serverds);
-			t.start();
+				
+			for(int i=0;i<3;i++){
+				// Cria uma nova thread para atender uma conexão
+				Thread t = new ClienteHandler();
+				t.start();
+			}	
 
 		} catch (Exception e) // SE OCORRER ALGUMA EXCESSAO, ENTAO DEVE SER TRATADA (AMIGAVELMENTE)
 		{
 			System.out.println("-S- O seguinte problema ocorreu : \n" + e.toString());
 		}
 	}
+
 }
 
 class ClienteHandler extends Thread {
@@ -29,16 +26,20 @@ class ClienteHandler extends Thread {
 	private String tipoUser;
 	private int idClient;
 	private boolean ocupado; // define se o client ja foi associado a outro client
-	final DatagramSocket ds;
-	private static int serverPort = 6000;
+	//final DatagramSocket ds;
+	private static int serverPort;
 
-	public ClienteHandler(DatagramSocket ds) {
-		this.ds = ds;
-		this.serverPort++;
+	public ClienteHandler() {
+		
 	}
 
-	public void run() {
+	//Impede que as threads acessem simultaneamente a conexão inicial, prevenindo assim congestionamento na porta
+	public static synchronized int conexaoInicial(int ServerPort, int newPort){
 		try {
+			// ESTABELECE UM SERVIÇO UDP NA PORTA ESPECIFICADA
+			DatagramSocket ds = new DatagramSocket(ServerPort);
+			System.out.println("-S- Servidor estabelecendo servico UDP (P:" + ServerPort + ")...");
+
 			// CRIA UM PACOTE
 			byte[] bytRec = new byte[100];
 			// RECEBE DADOS DO CLIENTE
@@ -55,7 +56,53 @@ class ClienteHandler extends Thread {
 			InetAddress ipRet = pktRec.getAddress();
 			int portaRet = pktRec.getPort();
 
-			// CRIA UM PACOTE de RETORNO
+			// CRIA UM PACOTE de RETORNO -> adicionar informações da porta nova que o client pode se conectar
+			String strMsgRet = newPort+"";
+			byte[] bytRet = strMsgRet.getBytes();
+			DatagramPacket pktEnv = new DatagramPacket(bytRet, bytRet.length, ipRet, portaRet);
+
+			System.out.println("DEBUG "+strMsgRet);
+
+			// E ENVIA PARA O CLIENTE
+			System.out.println(
+					"-S- Enviando mensagem (IP:" + ipRet.getHostAddress() + " - P:" + portaRet + " )...:" + strMsgRet);
+			ds.send(pktEnv);
+
+			// FINALIZA O SERVICO UDP
+			ds.close();
+			System.out.println("-S- Conexao finalizada...");
+
+		} catch (Exception e) // SE OCORRER ALGUMA EXCESSAO, ENTAO DEVE SER TRATADA (AMIGAVELMENTE)
+		{
+			System.out.println("-S- O seguinte problema ocorreu : \n" + e.toString());
+		}
+		return newPort;
+	}
+
+	public static void conexao(int serverPort){
+		try {
+			
+			//ESTABELECE UM SERVIÇO UDP NA NOVA PORTA ESPECIFICADA
+			DatagramSocket ds = new DatagramSocket(serverPort);
+			System.out.println("-S- Servidor estabelecendo servico UDP (P:" + serverPort + ")...");
+
+			// CRIA UM PACOTE
+			byte[] bytRec = new byte[100];
+			// RECEBE DADOS DO CLIENTE
+			DatagramPacket pktRec = new DatagramPacket(bytRec, bytRec.length);
+			System.out.println("-S- Recebendo mensagem...");
+			ds.receive(pktRec);
+
+			// PROCESSA O PACOTE RECEBIDO
+			bytRec = pktRec.getData();
+			String strMsg = new String(bytRec, 0, bytRec.length);
+			System.out.println("-S- Mensagem recebida: " + strMsg);
+
+			// Pega dados do Cliente
+			InetAddress ipRet = pktRec.getAddress();
+			int portaRet = pktRec.getPort();
+
+			// CRIA UM PACOTE de RETORNO -> adicionar informações da porta nova que o client pode se conectar
 			String strMsgRet = "RETORNO - " + strMsg;
 			byte[] bytRet = strMsgRet.getBytes();
 			DatagramPacket pktEnv = new DatagramPacket(bytRet, bytRet.length, ipRet, portaRet);
@@ -68,6 +115,22 @@ class ClienteHandler extends Thread {
 			// FINALIZA O SERVICO UDP
 			ds.close();
 			System.out.println("-S- Conexao finalizada...");
+
+		} catch (Exception e) // SE OCORRER ALGUMA EXCESSAO, ENTAO DEVE SER TRATADA (AMIGAVELMENTE)
+		{
+			System.out.println("-S- O seguinte problema ocorreu : \n" + e.toString());
+		}
+	}	
+
+	public void run() {
+		try {
+			//gera um número aleatório para ser uma porta disponível
+			int newPort = (int)(Math.random() * (10000 - 1 + 1) + 1);
+
+			//abre a conexao recepcionista, para trocar mensagem com o cliente sobre qual porta ele deverá usar
+			int handlerPort = conexaoInicial(6000,newPort);
+			conexao(handlerPort);
+			
 
 		} catch (Exception e) // SE OCORRER ALGUMA EXCESSAO, ENTAO DEVE SER TRATADA (AMIGAVELMENTE)
 		{
--- Usuario.java
+++ Usuario.java
@@ -2,11 +2,10 @@
 import java.net.*;
 import java.io.*;
 import java.util.Scanner;
-import java.util.Random;
 
 public class Usuario {
 
-    // M�TODO PRINCIPAL DA CLASSE
+    // MÉTODO PRINCIPAL DA CLASSE
 	public static void main(String args[]) {
 		try {
 
@@ -28,7 +27,8 @@ public class Usuario {
 					exit = true;
 					break;
 				case 1:
-					conexao(idClient,tipoUser,ocupado);
+					int newPort = conexaoInicial(idClient,tipoUser,ocupado);
+					conexao(idClient,tipoUser,ocupado,newPort);
 					break;
 				default:
 					System.out.println("comando não reconhecido");
@@ -42,7 +42,7 @@ public class Usuario {
 		}
 	}
 
-	public static void conexao(int idClient, String tipoUser,boolean ocupado) throws Exception {
+	public static int conexaoInicial(int idClient, String tipoUser,boolean ocupado) throws Exception {
         System.out.println("Procurando motorista...");
 		
 		// ENDEREÇO DO SERVIDOR
@@ -73,10 +73,63 @@ public class Usuario {
 		String strRet = new String(bytRec, 0, bytRec.length);
 
 		// PROCESSA O PACOTE RECEBIDO
+		//System.out.println("DEBUG |" + strRet+"|");
+		strRet = strRet.trim(); // remove os espaços em branco da string recebida
+		int newPort = Integer.parseInt(strRet);
+
+		// FINALIZA O SERVIÇO UDP
+		ds.close();
+		System.out.println("-C- Conexao finalizada...");
+
+		return newPort; // retorna o valor da porta
+	}
+
+	public static int conexao(int idClient, String tipoUser,boolean ocupado, int newPort) throws Exception {
+        System.out.println("Procurando motorista...");
+		
+		// ENDEREÇO DO SERVIDOR
+		String IPServidor = "25.19.211.24";
+		int PortaServidor = newPort;
+		int PortaCliente = 6002;
+
+		// ESTABELECE UM SERVIÇO UDP NA PORTA ESPECIFICADA
+		DatagramSocket ds = new DatagramSocket(PortaCliente);
+		System.out.println("-C- Cliente estabelecendo servico UDP (P" + PortaCliente + ")...");
+
+		// CRIA UM PACOTE E ENVIA PARA O SERVIDOR
+		String strEnvio = idClient+"/"+tipoUser+"/"+ocupado;
+		byte[] bytEnvio = strEnvio.getBytes();
+		DatagramPacket pktEnvio = new DatagramPacket(bytEnvio, bytEnvio.length, InetAddress.getByName(IPServidor),
+				PortaServidor);
+		System.out.println("-C- Enviando mensagem (IP:" + IPServidor + " - P:" + PortaServidor + ")...:" + strEnvio);
+		ds.send(pktEnvio);
+
+        //paramos aqui
+
+		// CRIA UM PACOTE E RECEBE DADOS DO SERVIDOR
+		byte[] bytRec = new byte[100];
+		DatagramPacket pktRec = new DatagramPacket(bytRec, bytRec.length);
+		System.out.println("-C- Recebendo mensagem...");
+		ds.receive(pktRec);
+		bytRec = pktRec.getData();
+		String strRet = new String(bytRec, 0, bytRec.length);
+
+		// PROCESSA O PACOTE RECEBIDO
 		System.out.println("-C- Mensagem recebida: " + strRet);
 
 		// FINALIZA O SERVIÇO UDP
 		ds.close();
 		System.out.println("-C- Conexao finalizada...");
+
+		return 0; // retorna o valor da porta
 	}
+
+	/*
+	public String lerPacote(byte[] pacote){
+		String retorno = "";
+		for(int i=0;i<pacote.length;i++){
+			retorno+=pacote[i];
+		}
+		return retorno;
+	} */
 }
